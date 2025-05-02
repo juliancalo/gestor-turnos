d1ee28f4c2ecc4372d6d8dd0392b8993c492fd41
405795-CALO <405795@tecnicatura.frc.utn.edu.ar>
2025-05-02 20:36:46 -0300

    First commit

---
+++ src/main/java/org/project/gestor/App.java
@@ -0,0 +1,13 @@
+package org.project.gestor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

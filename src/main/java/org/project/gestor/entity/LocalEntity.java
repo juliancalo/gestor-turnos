25165e6793f31de4025a7a8fa57fb386e79ac07a
405795-CALO <405795@tecnicatura.frc.utn.edu.ar>
2025-05-04 22:30:30 -0300

    Se agregan las clases Entidad Turnos y Local, además de Enum Estado y Categoría (Rubro).

---
+++ src/main/java/org/project/gestor/entity/LocalEntity.java
@@ -0,0 +1,24 @@
+package org.project.gestor.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.project.gestor.enums.Category;
+
+@Entity
+@Getter
+@Setter
+public class LocalEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private Category category;
+    private String address;
+    private String email;
+    private String phone;
+    private String officeHours;
+
+    @ManyToOne
+    private UserEntity user;
+}
